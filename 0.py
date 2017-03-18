ba6fc4f44d2e07a89f64a3a0972c4ff07696126b
jeff.tang <tangj1122@gmail.com>
2017-03-18 02:27:38 -0500

    Jeff learning Git

---
+++ 0.py
@@ -0,0 +1,8 @@
+
+def hacker_jeff():
+    kutub = 0
+    attendance = ['jeff', 'ben', 'albert', 'arvind']
+    if kutub not in attendance:
+        return 'if kutub not in attendance:\n\tgreiner = Poo Pants'
+
+print hacker_jeff()
