843fa12925a284edaa168613f66549ebccaacb22
Kutub Gandhi <kksgandhi@gmail.com>
2017-03-19 20:33:22 -0500

    add median_words, fails on green eggs and ham

--- analysis.py
+++ analysis.py
@@ -12,21 +12,41 @@ def findall_80s(text):
     return re.findall(r"\b(?:eighties|198\d|(?:19|)80s)\b",text,re.IGNORECASE)
 
 def count_distinct_words(filename):
-    url_file=urllib2.urlopen(codeskulptor.file2url(filename))
-    list_of_words=[]
-    for line in url_file.readlines():
-        for word in re.findall(r"\b\S+\b",line):
-            list_of_words.append(word)
+    list_of_words=getWords(filename)
     temp_set = set()
     for word in list_of_words:
         temp_set.add(word.lower())
     return len(temp_set)
 
+def getWords(filename):
+    url_file=urllib2.urlopen(codeskulptor.file2url(filename))
+    list_of_words=[]
+    for line in url_file.readlines():
+        for word in re.findall(r"\b\S+\b",line):
+            list_of_words.append(word)
+    return list_of_words
+
 def median_word(filename):
-    pass
+    list_of_words=[]
+    for word in getWords(filename):
+        print(word)
+        list_of_words.append(word.lower())
+    dict={}
+    for word in list_of_words:
+        if word in dict:
+            dict[word]+=1
+        else:
+            dict[word]=1
+    value_list=[]
+    for key in dict:
+        value_list.append((key,dict[key]))
+    value_list.sort(key=lambda x: x[1])
+    return value_list[len(value_list)/2][0]
+
+
 
 def wordseq_successor_counts(filename_list, seq_size, include_punc, is_case_sensitive):
-    pass
+
 
 def wordseq_successor_frequencies(filename_list, seq_size, include_punc, is_case_sensitive):
     pass
