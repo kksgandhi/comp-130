68f6b56627bd46ee1cb93730fcf062a630ee9ad8
Kutub Gandhi <kksgandhi@gmail.com>
2017-03-20 10:49:13 -0500

    massive code refactor for analysis.py, getWords now includes case sensitivity and puncutation. New regex, given by Greiner. All functions updated to use new getWords

--- 1.py
+++ 1.py
@@ -1,6 +1,5 @@
-import re
-
-str ="This is a strangely spaced\tsample.\n\nIt is a string."
-print(re.split(r"\s*",str))
-print(re.split(r"\s+",str))
-print(str.split())
+from sets import Set
+s = Set([1,2,3,4])
+t=Set([3,4,5,6])
+print(s.intersection(s.union(t)))
+print(t.union(s))
--- analysis.py
+++ analysis.py
@@ -12,23 +12,32 @@ def findall_80s(text):
     return re.findall(r"\b(?:eighties|198\d|(?:19|)80s)\b",text,re.IGNORECASE)
 
 def count_distinct_words(filename):
-    list_of_words=getWords(filename)
+    list_of_words=getWords(filename,False,False)
     temp_set = set()
     for word in list_of_words:
         temp_set.add(word.lower())
     return len(temp_set)
 
-def getWords(filename):
+def getWords(filename,include_punc, case_sensitive):
     url_file=urllib2.urlopen(codeskulptor.file2url(filename))
     list_of_words=[]
     for line in url_file.readlines():
-        for word in re.findall(r"\b[\S']+\b",line):
-            list_of_words.append(word)
+        for word in re.findall(r"[?!]+|-+|\.+|[,:;\"'`()/&#]|-?[A-Za-z0-9]+(?:['\-.@/][A-Za-z0-9]+)*|-?\$?\d{1,3}(?:,?\d{3})*(?:\.\d+)?",line):
+            if(case_sensitive):
+                list_of_words.append(word)
+            else:
+                list_of_words.append(word.lower())
+    if( not include_punc):
+        temp_list=[]
+        for word in list_of_words:
+            if re.match("[a-zA-Z]",word):
+                temp_list.append(word)
+        return temp_list
     return list_of_words
 
 def median_word(filename):
     list_of_words=[]
-    for word in getWords(filename):
+    for word in getWords(filename,False,False):
         print(word)
         list_of_words.append(word.lower())
     dict={}
@@ -43,28 +52,12 @@ def median_word(filename):
     value_list.sort(key=lambda x: x[1])
     return value_list[len(value_list)/2][0]
 def wordseq_successor_counts(filename_list, seq_size, include_punc, is_case_sensitive):
-    list_of_lists=[]
+    list_of_word_lists=[]
     for file in filename_list:
-        url_file=urllib2.urlopen(codeskulptor.file2url(file))
-        list_of_words=[]
-        for line in url_file.readlines():
-            print(line)
-            if(include_punc):
-                for word in re.findall(r"\b[\S']+\b|[!,.:;?][^.]",line):
-                    list_of_words.append(word)
-            else:
-                for word in re.findall(r"\b[\S']+(?:\b|')",line):
-                    print(word)
-                    list_of_words.append(word)
-        if(is_case_sensitive):
-            temp_list=[]
-            for word in list_of_words:
-                temp_list.append(word.lower())
-            list_of_words=temp_list
-        list_of_lists.append(list_of_words)
-
+        list_of_word_lists.append(getWords(file,include_punc,is_case_sensitive))
 
 print(wordseq_successor_counts(['comp130_AmericanPie.txt'], 1, False, False))
+
 def wordseq_successor_frequencies(filename_list, seq_size, include_punc, is_case_sensitive):
     pass
 
