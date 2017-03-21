c3a979127832ff8e50afdd54989a2619f3f59b99
Kutub Gandhi <kksgandhi@gmail.com>
2017-03-21 09:44:21 -0500

    add docstrings, code beautification

--- analysis.py
+++ analysis.py
@@ -1,3 +1,10 @@
+"""
+This program analyses (spelling is hard) text files.
+It has a function to find any word with sl
+It has a function that finds words with triple vowel
+It has a function that counts distinct words in a file
+It has a markhov chain generator, and a frequency generator
+"""
 import re
 import urllib2
 import codeskulptor
@@ -6,26 +13,42 @@ from collections import Counter
 
 
 def findall_sl(text):
+    """
+    Finds words that begin with sl
+    """
     return re.findall(r"\b[sS]l\S+\b", text)
 
 
 def findall_triple_vowel(text):
+    """
+    Finds words with triple vowels
+    """
     return re.findall(r"\b\S*(?:aaa+|eee+|iii+|ooo+|uuu+)\S*\b", text, re.IGNORECASE)
 
 
 def findall_80s(text):
+    """
+    Finds words that reference the eighties
+    """
     return re.findall(r"\b(?:eighties|198\d|(?:19|)80s)\b", text, re.IGNORECASE)
 
 
 def count_distinct_words(filename):
-    list_of_words = getWords(filename, False, False)
+    """
+    Counts the number of words in a file
+    """
+    list_of_words = get_word_list(filename, False, False)
     temp_set = set()
     for word in list_of_words:
         temp_set.add(word.lower())
     return len(temp_set)
 
 
-def getWords(filename, include_punc, case_sensitive):
+def get_word_list(filename, include_punc, case_sensitive):
+    """
+    Helper function I wrote, returns a list of words out of a file
+    Options to include_punctuation or return words case sensitive
+    """
     url_file = urllib2.urlopen(codeskulptor.file2url(filename))
     list_of_words = []
     for line in url_file.readlines():
@@ -44,16 +67,20 @@ def getWords(filename, include_punc, case_sensitive):
 
 
 def median_word(filename):
-    list_of_words = getWords(filename, False, False)
-    dict = {}
+    """
+    Returns the word that is mentioned a median amount of times in a file
+    """
+    list_of_words = get_word_list(filename, False, False)
+    # Yes I know this should be a Counter. Oh well
+    temp_dict = {}
     for word in list_of_words:
-        if word in dict:
-            dict[word] += 1
+        if word in temp_dict:
+            temp_dict[word] += 1
         else:
-            dict[word] = 1
+            temp_dict[word] = 1
     value_list = []
-    for key in dict:
-        value_list.append((key, dict[key]))
+    for key in temp_dict:
+        value_list.append((key, temp_dict[key]))
     value_list.sort(key=lambda x: x[1])
     return value_list[(len(value_list) / 2) - 1][0]
 
@@ -63,11 +90,17 @@ def wordseq_successor_counts(
      seq_size,
      include_punc,
      is_case_sensitive):
+    """
+    Creates a markhov chain using a list of files.
+    The chain consists of a dictionary mapping tuples to counters
+    The counter matches single word strings to
+    the amount of times that word has been mentioned after the tuple
+    """
     list_of_word_lists = []
     return_dict = defaultdict(Counter)
-    for file in filename_list:
+    for single_file in filename_list:
         list_of_word_lists.append(
-            getWords(file, include_punc, is_case_sensitive))
+            get_word_list(single_file, include_punc, is_case_sensitive))
     for word_list in list_of_word_lists:
         for index in range(0, len(word_list) - seq_size):
             temp_list = []
@@ -85,6 +118,9 @@ def wordseq_successor_frequencies(
      seq_size,
      include_punc,
      is_case_sensitive):
+    """
+    calls wordseq_successor_counts and converts the counts into frequencies
+    """
     counts_dict = wordseq_successor_counts(
         filename_list,
         seq_size,
